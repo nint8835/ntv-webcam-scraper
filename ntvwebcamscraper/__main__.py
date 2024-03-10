3b1a30cfdd3591965db24fbb5e66b3cb1600b316
Riley Flynn <riley@rileyflynn.me>
2024-03-10 19:22:08 -0230

    Typer CLI

--- ntvwebcamscraper/__main__.py
+++ ntvwebcamscraper/__main__.py
@@ -1,13 +1,29 @@
 import time
 
+import typer
 from scheduler import Scheduler
 
 from .config import config
 from .webcams import save_all_camera_images
 
-schedule = Scheduler()
-schedule.cyclic(config.interval, save_all_camera_images)
+app = typer.Typer()
 
-while True:
-    schedule.exec_jobs()
-    time.sleep(1)
+
+@app.command()
+def scrape():
+    """Scrape the webcam images once."""
+    save_all_camera_images()
+
+
+@app.command()
+def run():
+    """Begin scraping the webcam images at the specified interval."""
+    schedule = Scheduler()
+    schedule.cyclic(config.interval, save_all_camera_images)
+
+    while True:
+        schedule.exec_jobs()
+        time.sleep(1)
+
+
+app()
